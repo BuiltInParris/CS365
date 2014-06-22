c2ec447df0d95c0720ffbc74db20909c0339f9ea
Gaylord Holder <gholder@cs.drexel.edu>
2014-06-22 16:48:55 -0400

    added group check to first unit test

--- Ansible/roles/setup-accounts/files/unit_tests/ut-create-user.sh
+++ Ansible/roles/setup-accounts/files/unit_tests/ut-create-user.sh
@@ -9,14 +9,22 @@ function getMaxUID {
     getent passwd | sort -nr  -k +3 -t : | grep -v nobody | head -n 1 | cut -f 3 -d :
 }
 
+function getMaxGID {
+    getent group | sort -nr  -k +3 -t : | grep -v nogroup | head -n 1 | cut -f 3 -d :
+}
+
 OldUid=$(getMaxUID)
+OldGid=$(getMaxGID)
 User=gmcfoo-$((OldUid + 1))
+
 useradd "$User"
 
 NewUid=$(getMaxUID)
+NewGid=$(getMaxGID)
+
 HDir=$(getent passwd "$User" | cut -f 6 -d :)
 
-if [ $(($OldUid + 1)) = "$NewUid" ] && [ ! -d "$HDir" ]
+if [ $(($OldUid + 1)) == "$NewUid" ] && [ $((OldGid + 1)) == "$NewGid" ] && [ ! -d "$HDir" ]
 then
     echo $0 - OK
 else
