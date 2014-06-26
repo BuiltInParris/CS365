f13626feed2f7ce0b931cb1cdf572088ad69abab
Gaylord Holder <gholder@cs.drexel.edu>
2014-06-26 14:31:04 -0400

    rewrite of libuseradd.sh

--- Ansible/roles/nss-mysql-clients/files/libuseradd_mysql.sh
+++ Ansible/roles/nss-mysql-clients/files/libuseradd_mysql.sh
@@ -1,19 +1,128 @@
-	setUserLastDayMySQL "$User" "$opt_d"
-	setUserExpireDateMySQL "$User" "$opt_E"
-	setUserInactiveMySQL "$User" "$opt_I"
-	getUserAgingMySQL "$User" "$opt_l"
-	setUserMinDaysMySQL "$User" "$opt_m"
-	setUserMaxDaysMySQL "$User" "$opt_M"
-	setUserWarnDaysMySQL "$User" "$opt_W"
-addUser_MySQL "$UName" "$EPW" "$Uid" "$Gid" "$GeCOS" "$HDir" "$SHELL"
-addGroup_MySQL "$GName" "$Gid"
-rmUserMySQL "$User"
-rmUserFromGroupsMySQL "$User" 
-rmGroupMySQL "$User"
-	setUserGeCOSMySQL "$User" "$opt_c"
-	setUserHDirMySQL "$User" "$opt_d"
-	setUserGidMySQL "$User" "$opt_g"
-	setUserUNameMySQL "$User" "$opt_l"
-	setUserEPWMySQL "$User" "$opt_p"
-	setUserShellMySQL "$User" "$opt_s"
-	setUserUidMySQL "$User" "$opt_u"
+#!/bin/bash
+
+cmd=${cmd:=libuseradd.sh}
+
+LibNssMySQLCFG=/etc/libnss-mysql.cfg
+
+function warning {
+	echo "$@" 1>&2
+}
+
+if [ ! -e "$LibNssMySQLCFG" ]
+then
+	warning "$cmd : '$LibNssMySQLCFG' is missing.  Cannot find database."
+	exit 1
+fi
+
+MySQLHost=$(grep host "$LibNssMySQLCFG" | awk '${print $2}')
+MySQLDB=$(grep database "$LibNssMySQLCFG" | awk '${print $2}')
+
+function sys {
+	[ -n "${opt_n}${opt_v}" ] && echo "$@"
+	[ -z "$opt_n" ] || "$@"
+}
+
+function cmdMySQL {
+	mysql -h "$MySQLHost" -e "$@" "$MySQLDB"
+}
+
+function verbCmdMySQL {
+        [ -n "${opt_n}${opt_v}" ] && echo mysql -h "$MySQLHost" -e "$@" "$MySQLDB"
+        [ -z "$opt_n" ] || cmdMySQL "$@"
+}
+
+#addUser_MySQL "$UName" "$EPW" "$Uid" "$Gid" "$GeCOS" "$HDir" "$SHELL"
+function addUser_MySQL {
+	local User="$1"
+}
+
+# setUserGeCOSMySQL "$User" "$opt_c"
+function setUserGeCOSMySQL {
+	local User="$1"
+}
+
+# setUserHDirMySQL "$User" "$opt_d"
+function setUserHDirMySQL {
+	local User="$1"
+}
+
+# setUserGidMySQL "$User" "$opt_g"
+function setUserGidMySQL {
+	local User="$1"
+}
+
+# setUserUNameMySQL "$User" "$opt_l"
+function setUserUNameMySQL {
+	local User="$1"
+}
+
+# setUserEPWMySQL "$User" "$opt_p"
+function setUserEPWMySQL {
+	local User="$1"
+}
+
+# setUserShellMySQL "$User" "$opt_s"
+function setUserShellMySQL {
+	local User="$1"
+}
+
+# setUserUidMySQL "$User" "$opt_u"
+function setUserUidMySQL {
+	local User="$1"
+}
+
+# setUserLastDayMySQL "$User" "$opt_d"
+function setUserLastDayMySQL {
+	local User="$1"
+}
+
+# setUserExpireDateMySQL "$User" "$opt_E"
+function setUserExpireDateMySQL {
+	local User="$1"
+}
+		
+# setUserInactiveMySQL "$User" "$opt_I"
+function setUserInactiveMySQL {
+	local User="$1"
+}
+
+# getUserAgingMySQL "$User" "$opt_l"
+function getUserAgingMySQL {
+	local User="$1"
+}
+
+# setUserMinDaysMySQL "$User" "$opt_m"
+function setUserMinDaysMySQL {
+	local User="$1"
+}
+
+# setUserMaxDaysMySQL "$User" "$opt_M"
+function setUserMaxDaysMySQL {
+	local User="$1"
+}
+
+# setUserWarnDaysMySQL "$User" "$opt_W"
+function setUserWarnDaysMySQL {
+	local User="$1"
+}
+
+#addGroup_MySQL "$GName" "$Gid"
+function addGroup_MySQL {
+	local User="$1"
+}
+
+#rmUserMySQL "$User"
+function rmUserMySQL {
+	local User="$1"
+}
+
+#rmUserFromGroupsMySQL "$User" 
+function rmUserFromGroupsMySQL {
+	local User="$1"
+}
+
+#rmGroupMySQL "$User"
+function rmGroupMySQL {
+	local User="$1"
+}
+
