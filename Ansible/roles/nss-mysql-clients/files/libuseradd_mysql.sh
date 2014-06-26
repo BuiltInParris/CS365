b049e21c39e137aff8cf1f9c8fd37db1c23638a9
Gaylord Holder <gholder@cs.drexel.edu>
2014-06-26 18:06:32 -0400

    first full cut of libuseradd_mysql - more debugging needed

--- Ansible/roles/nss-mysql-clients/files/libuseradd_mysql.sh
+++ Ansible/roles/nss-mysql-clients/files/libuseradd_mysql.sh
@@ -3,6 +3,15 @@
 cmd=${cmd:=libuseradd.sh}
 
 LibNssMySQLCFG=/etc/libnss-mysql.cfg
+AddUserConf=/etc/adduser.conf
+LoginDefs=/etc/login.defs
+
+MYHOME=$(grep '^DHOME=' "$AddUserConf" | cut -f 2 -d =)
+SKEL=$(grep '^SKEL=' "$AddUserConf" | cut -f 2 -d =)
+UID_MIN=$(grep '^UID_MIN=' "$LoginDefs" | cut -f 2 -d =)	
+GID_MIN=$(grep '^GID_MIN=' "$LoginDefs" | cut -f 2 -d =)	
+SYS_UID_MAX=$((UID_MIN - 1))
+SYS_GID_MAX=$((GID_MIN - 1))
 
 function warning {
 	echo "$@" 1>&2
@@ -18,97 +27,177 @@ MySQLHost=$(grep host "$LibNssMySQLCFG" | awk '{print $2}')
 MySQLDB=$(grep database "$LibNssMySQLCFG" | awk '{print $2}')
 
 function sys {
-	[ -n "${opt_n}${opt_v}" ] && echo "$@"
-	[ -z "$opt_n" ] || "$@"
+	[ -n "${opt_n}${opt_v}" ] && warning "$@"
+	[ -n "$opt_n" ] || "$@"
 }
 
 function cmdMySQL {
-	mysql -NB -h "$MySQLHost" -e "$@" "$MySQLDB"
+	if [ -n "$*" ]
+	then
+	    mysql -NB -h "$MySQLHost" -e "$@" "$MySQLDB"
+	else
+	    mysql -NB -h "$MySQLHost" "$MySQLDB"
+	fi
 }
 
 function verbCmdMySQL {
-        [ -n "${opt_n}${opt_v}" ] && echo mysql -h "$MySQLHost" -e "$@" "$MySQLDB"
-        [ -z "$opt_n" ] || cmdMySQL "$@"
+        if [ -n "$opt_n" ]
+	then
+	    if [ -n "$*" ]
+	    then
+		warning mysql -h "$MySQLHost" -e "$@" "$MySQLDB"
+	    else
+		cat
+		warning mysql -h "$MySQLHost" "$MySQLDB"
+	    fi
+	elif [ -n "$opt_v" ]
+	then
+		if [ -n "$*" ]
+		then
+			warning mysql -h "$MySQLHost" -e "$@" "$MySQLDB"
+			cmdMySQL "$@"
+		else
+			tee >(cmdMySQL)
+			warning mysql -h "$MySQLHost" "$MySQLDB"
+			warning ""
+		fi
+	else
+	 	cmdMySQL "$@"
+	fi
+
 }
 
 #addUser_MySQL "$UName" "$EPW" "$Uid" "$Gid" "$GeCOS" "$HDir" "$SHELL"
 function addUser_MySQL {
-	local User="$1"
+	local UName="$1"
+	local EPW="$2"
+	local Uid="$3"
+	local Gid="$4"
+	local GeCOS="$5"
+	local HDir="$6"
+	local Shell="$7"
+
+	verbCmdMySQL "INSERT INTO users (username,password,uid,gid,gecos,homedir,shell)
+			    VALUES ('$UName', '$EPW', $Uid, $Gid, '$GeCOS', '$HDir', '$Shell')"
 }
 
 # setUserGeCOSMySQL "$User" "$opt_c"
 function setUserGeCOSMySQL {
 	local User="$1"
+	local GeCOS="$2"
+
+	verbCmdMySQL "UPDATE users SET gecos = '$GeCOS' WHERE username = '$User'"
 }
 
 # setUserHDirMySQL "$User" "$opt_d"
 function setUserHDirMySQL {
 	local User="$1"
+	local HDir="$2"
+
+	verbCmdMySQL "UPDATE users SET homedir = '$HDir' WHERE username = '$User'"
 }
 
 # setUserGidMySQL "$User" "$opt_g"
 function setUserGidMySQL {
 	local User="$1"
+	local Gid="$2"
+
+	verbCmdMySQL "UPDATE users SET gid = '$Gid' WHERE username = '$User'"
 }
 
 # setUserUNameMySQL "$User" "$opt_l"
 function setUserUNameMySQL {
-	local User="$1"
+	local OldUserName="$1"
+	local NewUserName="$2"
+
+	verbCmdMySQL "UPDATE users SET username = '$NewUserName' WHERE username = '$OldUserName'"
 }
 
 # setUserEPWMySQL "$User" "$opt_p"
 function setUserEPWMySQL {
 	local User="$1"
+	local EPW="$2"
+
+	verbCmdMySQL "UPDATE users SET password = '$EPW' WHERE username = '$User'"
 }
 
 # setUserShellMySQL "$User" "$opt_s"
 function setUserShellMySQL {
 	local User="$1"
+	local Shell="$2"
+
+	verbCmdMySQL "UPDATE users SET shell = '$Shell' WHERE username = '$User'"
 }
 
 # setUserUidMySQL "$User" "$opt_u"
 function setUserUidMySQL {
 	local User="$1"
+	local Uid="$2"
+
+	verbCmdMySQL "UPDATE users SET uid = '$Uid' WHERE username = '$User'"
 }
 
 # setUserLastDayMySQL "$User" "$opt_d"
 function setUserLastDayMySQL {
 	local User="$1"
+	local LastDay="$2"
+
+	verbCmdMySQL "UPDATE users SET lstchg = '$LastDay' WHERE username = '$User'"
 }
 
 # setUserExpireDateMySQL "$User" "$opt_E"
 function setUserExpireDateMySQL {
 	local User="$1"
+	local ExpireDate="$2"
+
+	verbCmdMySQL "UPDATE users SET expire = '$ExpireDate' WHERE username = '$User'"
 }
 		
 # setUserInactiveMySQL "$User" "$opt_I"
 function setUserInactiveMySQL {
 	local User="$1"
-}
+	local InActive="$2"
 
-# getUserAgingMySQL "$User" "$opt_l"
-function getUserAgingMySQL {
-	local User="$1"
+	verbCmdMySQL "UPDATE users SET inactive = '$Inactive' WHERE username = '$User'"
 }
 
 # setUserMinDaysMySQL "$User" "$opt_m"
 function setUserMinDaysMySQL {
 	local User="$1"
+	local Days="$2"
+
+	verbCmdMySQL "UPDATE users SET min = '$Days' WHERE username = '$User'"
 }
 
 # setUserMaxDaysMySQL "$User" "$opt_M"
 function setUserMaxDaysMySQL {
 	local User="$1"
+	local Days="$2"
+
+	verbCmdMySQL "UPDATE users SET max = '$Days' WHERE username = '$User'"
 }
 
 # setUserWarnDaysMySQL "$User" "$opt_W"
 function setUserWarnDaysMySQL {
 	local User="$1"
+	local Days="$2"
+
+	verbCmdMySQL "UPDATE users SET warn= '$Days' WHERE username = '$User'"
+}
+
+# getUserAgingMySQL "$User" "$opt_l"
+function getUserAgingMySQL {
+	local User="$1"
+
+	verbCmdMySQL "select lstchg,min,max,warn,inact,expire FROM users WHERE username = '$User'"
 }
 
 #addGroup_MySQL "$GName" "$Gid"
 function addGroup_MySQL {
-	local User="$1"
+	local Group="$1"
+	local Gid="$2"
+	
+	verbCmdMySQL "INSERT INTO groups (name,gid) VALUES ('$Group',$Gid)"
 }
 
 #rmUserMySQL "$User"
@@ -119,11 +208,15 @@ function rmUserMySQL {
 #rmUserFromGroupsMySQL "$User" 
 function rmUserFromGroupsMySQL {
 	local User="$1"
+
+	verbCmdMySQL "DELETE FROM grouplist where username = '$User'"
 }
 
 #rmGroupMySQL "$User"
 function rmGroupMySQL {
-	local User="$1"
+	local Group="$1"
+
+	verbCmdMySQL "DELETE FROM group where name = '$Group'"
 }
 
 function getNextGid {
@@ -150,8 +243,47 @@ function getNextUid {
 	echo $(($MaxUID + 1))
 }
 
+function getNextSysUid {
+	local MaxUID=$(cmdMySQL "SELECT uid FROM users WHERE uid < $UID_MAX ORDER BY uid DESC limit 1")
+
+	if [ -z "$MaxUID" ]
+	then
+		warning "No uids in users table"
+		return 1
+	fi
+
+	echo $(($MaxUID + 1))
+}
+
+function getNextSysGid {
+	local MaxUID=$(cmdMySQL "SELECT gid FROM group WHERE gid < $GID_MAX ORDER BY gid DESC limit 1")
+
+	if [ -z "$MaxGID" ]
+	then
+		warning "No gids in group table"
+		return 1
+	fi
+
+	echo $(($MaxGID + 1))
+}
+
+function addUserToGidMySQL {
+	local Gid="$1"
+	local User="$2"
+
+	verbCmdMySQL "INSERT INTO grouplist (gid,username) VALUES ($Gid,'$User')"
+}
+
 function addUsersToGroup {
 	local Group="$1"
+	local i
+
+	local Gid=$(cmdMySQL "SELECT gid FROM groups WHERE name = '$Group'")
+
+	for i in "$@"
+	do
+		addUserToGidMySQL "$Gid" "$i"
+	done
 }
 
 
